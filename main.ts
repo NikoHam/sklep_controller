2556d22a2e6458390b2bf889970f9f31eb855aa7
NikoHam <58589799+NikoHam@users.noreply.github.com>
2022-04-26 09:51:35 +0200

    ovladani fontanz a sprchz

--- main.ts
+++ main.ts
@@ -1,19 +1,37 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 9) {
         fontana = fontana * -1
-        if (fontana < 10) {
-            pins.digitalWritePin(DigitalPin.P0, 0)
+        if (fontana < 0) {
+            pins.digitalWritePin(DigitalPin.P11, 0)
         } else {
-            pins.digitalWritePin(DigitalPin.P0, 1)
+            pins.digitalWritePin(DigitalPin.P11, 1)
+        }
+    }
+    if (receivedNumber == 10) {
+        sprcha = sprcha * -1
+        if (sprcha < 0) {
+            pins.digitalWritePin(DigitalPin.P12, 0)
+        } else {
+            pins.digitalWritePin(DigitalPin.P12, 1)
         }
     }
 })
+let sprcha = 0
 let fontana = 0
 radio.setGroup(1)
 fontana = -1
-let sprcha = -1
+sprcha = -1
 basic.forever(function () {
-    if (pins.digitalReadPin(DigitalPin.P0) == 0) {
+    if (pins.digitalReadPin(DigitalPin.P1) == 0) {
+        radio.sendNumber(1)
+        radio.sendNumber(6)
+    }
+    if (pins.digitalReadPin(DigitalPin.P2) == 0) {
+        radio.sendNumber(2)
+        radio.sendNumber(7)
+    }
+    if (pins.digitalReadPin(DigitalPin.P8) == 0) {
+        radio.sendNumber(3)
         radio.sendNumber(8)
     }
 })
